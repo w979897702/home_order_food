e35f1715f785a67fecee68134afa889ff6db6cca
weixuzhao <weixuzhao@bytedance.com>
2025-09-20 13:36:24 +0800

    refactor(dishes): 将图片路径改为云存储前缀并删除本地图片文件
    删除不再使用的本地图片文件，并将菜品数据中的图片路径统一修改为云存储前缀，以优化资源管理和加载效率

--- miniprogram/data/dishes.js
+++ miniprogram/data/dishes.js
@@ -1,3 +1,4 @@
+const prefix = "cloud://cloud1-4go0vz3f4d21968a.636c-cloud1-4go0vz3f4d21968a-1379673983/"
 // 菜品数据
 const dishes = [
   // 川菜
@@ -5,25 +6,25 @@ const dishes = [
       id: 'd0101',
       categoryId: 'c01',
       name: '鱼香肉丝',
-      image: '/images/smaller/鱼香肉丝.webp',
+      image: `${prefix}鱼香肉丝.webp`,
     },
     {
       id: 'd0102',
       categoryId: 'c01',
       name: '青椒肉丝',
-      image: '/images/smaller/青椒肉丝.webp',
+      image: `${prefix}青椒肉丝.webp`,
     },
     {
       id: 'd0103',
       categoryId: 'c01',
       name: '小炒黄牛肉',
-      image: '/images/smaller/小炒黄牛肉.webp',
+      image: `${prefix}小炒黄牛肉.webp`,
     },
     {
       id: 'd0104',
       categoryId: 'c01',
       name: '麻婆豆腐',
-      image: '/images/smaller/麻婆豆腐.webp',
+      image: `${prefix}麻婆豆腐.webp`,
     }
   ],
   // 淮扬菜  c03
@@ -31,40 +32,40 @@ const dishes = [
     id: 'd0301',
     categoryId: 'c03',
     name: '糖醋排骨',
-    image: '/images/smaller/糖醋排骨.webp',
+    image: `${prefix}糖醋排骨.webp`,
   }, ],
   // 东北菜  c04
   ...[{
     id: 'd0401',
     categoryId: 'c04',
     name: '锅包肉',
-    image: '/images/smaller/锅包肉.webp',
+    image: `${prefix}锅包肉.webp`,
   }],
   // 素菜  c09
   ...[{
     id: 'd0901',
     categoryId: 'c09',
     name: '清炒油麦菜',
-    image: '/images/smaller/清炒油麦菜.jpg',
+    image: `${prefix}清炒油麦菜.jpg`,
   }],
   // 其他 c14
   ...[{
       id: 'd1401',
       categoryId: 'c14',
       name: '芥末虾球',
-      image: '/images/smaller/芥末虾球.webp',
+      image: `${prefix}芥末虾球.webp`,
     },
     {
       id: 'd1402',
       categoryId: 'c14',
       name: '可乐鸡翅',
-      image: '/images/smaller/可乐鸡翅.webp',
+      image: `${prefix}可乐鸡翅.webp`,
     },
     {
       id: 'd1403',
       categoryId: 'c14',
       name: '红烧肉',
-      image: '/images/smaller/红烧肉.webp',
+      image: `${prefix}红烧肉.webp`,
     }
   ],
   // 拌面 
@@ -72,7 +73,7 @@ const dishes = [
     id: 'd1101',
     categoryId: 'c11',
     name: '鲜椒牛肉拌面',
-    image: '/images/smaller/鲜椒牛肉拌面.jpg',
+    image: `${prefix}鲜椒牛肉拌面.jpg`,
   }]
 ];
 
